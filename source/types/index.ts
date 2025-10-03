965e102b10899ad990622ec7382f174bb4669161
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2025-10-03 18:41:43 -0500

    Delete source/types directory

--- source/types/index.ts
+++
@@ -1,9 +0,0 @@
-import type Logger from "@imjxsx/logger"
-
-export type Nullable<T> = T | undefined | null
-export interface LnOptions {
-  default: string
-  directory?: string
-  online?: boolean
-  logger?: Nullable<Logger>
-}
