13226c60ab060e1161d63546c9b3ac20eccd67e2
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2025-10-03 18:46:26 -0500

    Update index.d.ts

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,13 +1,25 @@
-import Logger from "@imjxsx/logger"
-import type { LnOptions, Nullable } from "./types/index.js";
-declare class Ln {
-    logger: Logger;
-    locales: Map<string, Map<string, string>>;
-    private default;
-    private directory;
-    constructor(options: LnOptions);
-    load(): Promise<void>;
-    t(key: string, language: string, vars?: Nullable<Record<string, string>>): string;
+import type Logger from "@imjxsx/logger"
+
+export type Nullable<T> = T | undefined | null
+
+export interface LnOptions {
+  default: string
+  directory?: string
+  online?: boolean
+  logger?: Nullable<Logger>
 }
-export default Ln;
-export { Ln, };
+
+export interface Ln {
+  t(textToTranslate: string, key: string, language?: string, vars?: Record<string, string>): Promise<string>
+  t(key: string, language?: string, vars?: Record<string, string>): Promise<string>
+  load(): Promise<void>
+  reset(): void
+}
+
+export class Ln {
+  constructor(options: LnOptions)
+  load(): Promise<void>
+  t(...args: any[]): Promise<string>
+  reset(): void
+}
+export default Ln
